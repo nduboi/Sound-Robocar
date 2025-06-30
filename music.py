1b749d642aa3e8cd2ed25422c1db50c049b5d351
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-06-30 15:35:15 +0200

    Add music playback functionality with circus horn sound

---
+++ music.py
@@ -0,0 +1,24 @@
+import pyaudio
+import wave
+
+filename = 'assets/circus_horn.mp3'
+
+chunk = 1024
+
+wf = wave.open(filename, 'rb')
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                channels = wf.getnchannels(),
+                rate = wf.getframerate(),
+                output = True)
+
+data = wf.readframes(chunk)
+
+while data != '':
+    stream.write(data)
+    data = wf.readframes(chunk)
+
+stream.close()
+p.terminate()
